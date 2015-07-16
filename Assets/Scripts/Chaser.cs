90c6e44f7acbf1797a4cd4efd14c85517a02c1a0
Andrew Montanez <amontane@mit.edu>
2015-07-15 21:48:59 -0700

    Here goes nothing

---
+++ Assets/Scripts/Chaser.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chaser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (.1f, 0);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (.1f, 0);
+
+	}
+}
--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -6,6 +6,10 @@ public class SpawnScript : MonoBehaviour {
 	public GameObject[] obj;
 	public float spawnMin = 1f;
 	public float spawnMax = 2f;
+	public float maxHeight = 1.7f;
+	public float minHeight = -4.1f;
+	public float maxSize = 7.5f;
+	public float minSize = 2.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +17,12 @@ public class SpawnScript : MonoBehaviour {
 	}
 
 	void Spawn(){
-		Instantiate(obj[Random.Range (0,obj.GetLength (0))], transform.position, Quaternion.identity);
+		Vector3 v = new Vector3(transform.position.x + Random.Range(minSize,maxSize), Random.Range(minHeight,maxHeight), transform.position.z);
+		//Instantiate(obj[Random.Range (0,obj.GetLength (0))], transform.position, Quaternion.identity);
+		//Instantiate(obj[Random.Range (0,obj.GetLength (0))], v, Quaternion.identity);
+		Object platform = Instantiate(obj[Random.Range (0,obj.GetLength (0))], v, Quaternion.identity);
+		//make sure that platforms are certain distance apart
+
 		Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
 	}
 }
